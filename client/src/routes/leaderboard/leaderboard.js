798c9856fb5edc0e9eaf71ede4a5aad5736522fb
Henry Boyer <HenryBoyerCodes@gmail.com>
2022-10-21 11:18:34 -0500

    remade the leaderboard to look better

--- client/src/routes/leaderboard/leaderboard.js
+++ client/src/routes/leaderboard/leaderboard.js
@@ -6,11 +6,40 @@ import CustomCheckbox from '../../assets/components/customCheckbox.js';
 import useAPI from '../../assets/hooks/useAPI.js';
 import LoadingOverlay from '../../assets/components/loadingOverlay.js';
 
+import './leaderboard.css';
+
+/* add dates to database */
+
 function Leaderboard(){
+  let [pageData, setPageData] = useState(null);
+
+
+  var table = null;
+  if (pageData === null){
+    table = (
+      <div>
+        cannot find any match
+      </div>
+    )
+  }else{
+    table = (
+      <table>
+        {pageData.map(stat => {
+          <tr>
+            <td>{stat.place}</td>
+            <td>{stat.score}</td>
+            <td>{stat.name}</td>
+            <td>{stat.date}</td>
+          </tr>
+        })}
+      </table>
+    )
+  }
+
   return (
-    <div>
-      test
-    </div>
+    <div className = 'page_content leaderboard'>
+      {table}
+    </div> 
   )
 }
 
