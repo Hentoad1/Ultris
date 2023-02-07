06faf348e7a0d164b872ac9b0a5e83e2ed364cb7
Henry Boyer <HenryBoyerCodes@gmail.com>
2023-02-07 03:09:10 -0600

    made client folder work properly in api folder

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.use(function(req,res,next){
 router.use('/verify', require('./verify/verify'));
 
 //ALL GET REQUESTS ARE GIVEN THE APP
-const build = path.resolve(__dirname + '/../../client/build');
+const build = path.resolve(__dirname + '/../client/build');
 router.get('*', async function(req, res, next) {
   res.sendFile(build + "/index.html");
 });
