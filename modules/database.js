09d711b896e945f143684ce134ceafee59b9c364
Henry Boyer <HenryBoyerCodes@gmail.com>
2023-02-11 15:03:12 -0600

    Update database.js

--- modules/database.js
+++ modules/database.js
@@ -36,14 +36,31 @@ function queryDB(...query){
   }
 
   return new Promise((resolve, reject) => {
-
-    let connection = mysql.createConnection(options);
+    
+    
+    mysql.createConnection(options).then(connection => {
+      connection.query(...query, function(err, result) {
+        connection.end();
+        if (err) return reject (err);
+  
+        resolve(result);
+      });
+    });
+    
+    
+    
+    
+    /*let connection = mysql.createConnection(options);
     connection.query(...query, function(err, result) {
       connection.end();
       if (err) return reject (err);
 
       resolve(result);
-    });
+    });*/
+
+
+
+
   });
 }
 
